95dc60c1ef4a1f6a5267940c52c81a761125571c
Stefano Fiumara <sdf10517@gmail.com>
2017-11-08 18:26:38 -0600

    Use tournament scoring in Competitive mode

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/Percentage.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/Percentage.lua
@@ -1,6 +1,7 @@
 local pn = ...
 
 local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn)
+local actualDP = stats:GetActualDancePoints()
 local PercentDP = stats:GetPercentDancePoints()
 local percent = FormatPercentScore(PercentDP)
 -- Format the Percentage string, removing the % symbol
@@ -21,7 +22,35 @@ return Def.ActorFrame{
 	LoadFont("_wendy white")..{
 		Text=percent,
 		Name="Percent",
-		InitCommand=cmd(valign, 1; horizalign, right; zoom,0.585 ),
+		InitCommand=cmd(valign, 1.2; horizalign, right; zoom,0.585 ),
 		OnCommand=cmd(x, 70)
+	},
+
+	LoadFont("_miso")..{
+		Text="Tournament Points: ",
+		Name="PointsLabel",
+		InitCommand=cmd(valign, -7; horizalign, left; zoom,0.45 ),
+		OnCommand=cmd(x, -24; y, -27)
+	},
+	Def.RollingNumbers {
+		Font="_wendy white",
+		InitCommand=function(self)
+			self:valign(-4)
+			self:horizalign(right)
+			self:zoom(0.15)
+			self:x(67)
+			self:Load("RollingNumbersEvaluationC")
+		end,
+		BeginCommand=function(self)
+			self:targetnumber(actualDP)
+		end
+
 	}
+	-- LoadFont("_wendy white")..{
+	-- 	--Text=actualDP,
+	-- 	Text=99999,
+	-- 	Name="Points",
+	-- 	InitCommand=cmd(valign, -5; horizalign, right; zoom,0.125 ),
+	-- 	OnCommand=cmd(x, 67)
+	-- }
 }
