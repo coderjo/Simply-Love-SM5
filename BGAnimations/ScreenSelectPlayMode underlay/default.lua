e5c268b710eba9f89763d5caba2e6e059820feef
StefanoFiumara <sdf10517@gmail.com>
2018-03-13 00:01:55 -0500

    Initial Implementation of ScreenSelectPlayMode

--- BGAnimations/ScreenSelectPlayMode underlay/default.lua
+++ BGAnimations/ScreenSelectPlayMode underlay/default.lua
@@ -15,24 +15,12 @@ local Update = function(af, delta)
 	local index = TopScreen:GetSelectionIndex( GAMESTATE:GetMasterPlayerNumber() )
 	if index ~= cursor.index then
 		cursor.index = index
-
-		-- queue the appropiate command to the faux playfield, if needed
-		if choices[cursor.index+1] == "Marathon" or choices[cursor.index+1] == "Regular" then
-			af:queuecommand("FirstLoop"..choices[cursor.index+1])
-		end
-
-		-- queue an "Update" to the AF containing the cursor, description text, score, and lifemeter actors
-		-- since they are children of that AF, they will also listen for that command
-		af:queuecommand("Update")
 	end
 end
 
 local t = Def.ActorFrame{
-	InitCommand=function(self)
-		self:SetUpdateFunction( Update )
-			:xy(_screen.cx+90, _screen.cy)
-			:zoom(1.25)
-	end,
+	InitCommand=cmd(SetUpdateFunction, Update),
+
 	OnCommand=function(self)
 		-- Get the Topscreen and its name, now that that TopScreen itself actually exists
 		TopScreen = SCREENMAN:GetTopScreen()
@@ -61,55 +49,11 @@ local t = Def.ActorFrame{
 		end
 	end,
 
-	-- side mask
-	Def.Quad{
-		InitCommand=function(self) self:zoomto(450, 450):diffuse(1,1,1,1):x(375):MaskSource() end
-	},
-	-- lower mask
-	Def.Quad{
-		InitCommand=function(self) self:zoomto(450, 450):diffuse(1,1,1,1):xy(74,305):MaskSource() end
-	},
-
-	-- gray backgrounds
-	Def.ActorFrame{
-		InitCommand=function(self) self:x(-188) end,
-		Def.Quad{
-			InitCommand=function(self) self:diffuse(0.2,0.2,0.2,1):zoomto(90,38):y(-60) end,
-			OffCommand=function(self) self:sleep(0.4):linear(0.1):diffusealpha(0) end
-		},
-		Def.Quad{
-			InitCommand=function(self) self:diffuse(0.2,0.2,0.2,1):zoomto(90,38):y(-20) end,
-			OffCommand=function(self) self:sleep(0.3):linear(0.1):diffusealpha(0) end
-		},
-		Def.Quad{
-			InitCommand=function(self) self:diffuse(0.2,0.2,0.2,1):zoomto(90,38):y(20) end,
-			OnCommand=function(self) if choices[3]==nil then self:visible(false) end end,
-			OffCommand=function(self) self:sleep(0.2):linear(0.1):diffusealpha(0) end
-		},
-		Def.Quad{
-			InitCommand=function(self) self:diffuse(0.2,0.2,0.2,1):zoomto(90,38):y(60) end,
-			OnCommand=function(self) if choices[4]==nil then self:visible(false) end end,
-			OffCommand=function(self) self:sleep(0.1):linear(0.1):diffusealpha(0) end
-		},
-	},
-
-	-- border
-	Def.Quad{
-		InitCommand=function(self) self:zoomto(302, 162):diffuse(1,1,1,1) end,
-		OffCommand=function(self) self:sleep(0.6):linear(0.2):cropleft(1) end
-	},
-	-- background
-	Def.Quad{
-		InitCommand=function(self) self:zoomto(300, 160):diffuse(0,0,0,1) end,
-		OffCommand=function(self) self:sleep(0.6):linear(0.2):cropleft(1) end
-	},
-
-
-	-- description
+	-- description TODO - Figure out description text animation
 	Def.BitmapText{
-		Font="_miso",
+		Font="titlemenu",
 		InitCommand=function(self)
-			self:zoom(0.825):halign(0):valign(0):xy(-130,-60)
+			self:zoom(0.55):halign(0):valign(0):xy(-130,-60)
 		end,
 		UpdateCommand=function(self)
 			self:settext( THEME:GetString("ScreenSelectPlayMode", choices[cursor.index+1] .. "Description") )
@@ -121,98 +65,22 @@ local t = Def.ActorFrame{
 	Def.ActorFrame{
 		Name="Cursor",
 		OnCommand=function(self)
-			-- it is possible for players to have something other than "Casual" as the default choice
-			-- for ScreenSelectPlayMode (see: Simply Love Options in the Operator Menu)
-			-- account for that here, in the OnCommand of the cursor ActorFrame, by updating cursor.index
-			-- to match the value of ThemePrefs.Get("DefaultGameMode") in the choices table
 			if ScreenName == "ScreenSelectPlayMode" then
 				cursor.index = (FindInTable(ThemePrefs.Get("DefaultGameMode"), choices) or 1) - 1
 			end
-			self:x(-150):y( -60 + (cursor.h * cursor.index) )
 		end,
-		UpdateCommand=function(self)
-			self:stoptweening():linear(0.1)
-				:y( -60 + (cursor.h * cursor.index) )
-		end,
-
-		Def.Quad{
-			InitCommand=function(self) self:zoomtoheight(cursor.h+2):diffuse(1,1,1,1):x(-1):halign(1) end,
-			UpdateCommand=function(self) self:zoomtowidth( clamp( choice_actors[cursor.index+1]:GetWidth()/1.4, cursor.min_w, cursor.max_w) ) end,
-			OffCommand=function(self) self:sleep(0.4):linear(0.2):cropleft(1) end
-		},
-		Def.Quad{
-			InitCommand=function(self) self:zoomtoheight(cursor.h):diffuse(0,0,0,1):halign(1) end,
-			UpdateCommand=function(self) self:zoomtowidth( clamp(choice_actors[cursor.index+1]:GetWidth()/1.4, cursor.min_w, cursor.max_w) ) end,
-			OffCommand=function(self) self:sleep(0.4):linear(0.2):cropleft(1) end
-		}
 	},
 
-	-- Score
-	Def.BitmapText{
-		Font="_wendy monospace numbers",
-		InitCommand=function(self)
-			self:zoom(0.225):xy(124,-68):diffusealpha(0)
-		end,
-		OffCommand=function(self) self:sleep(0.4):linear(0.2):diffusealpha(0) end,
-		UpdateCommand=function(self)
-			if ScreenName == "ScreenSelectPlayMode" then
-				if choices[cursor.index+1] == "Casual" then
-					self:stoptweening():linear(0.25):diffusealpha(0)
-				else
-					if choices[cursor.index+1] == "ECFA" then
-						self:settext("99.50")
-					else
-						self:settext("77.41")
-					end
-					self:stoptweening():linear(0.25):diffusealpha(1)
-				end
-			else
-				self:diffusealpha(1)
-				if SL.Global.GameMode == "ECFA" then
-					self:settext("99.50")
-				else
-					self:settext("77.41")
-				end
-			end
-		end,
-
-	},
-	-- LifeMeter
+	--Character
 	Def.ActorFrame{
-		Name="LifeMeter",
-		InitCommand=function(self) self:diffusealpha(0) end,
-		OffCommand=function(self) self:sleep(0.4):linear(0.2):diffusealpha(0) end,
-		UpdateCommand=function(self)
-			if ScreenName == "ScreenSelectPlayMode" then
-				if choices[cursor.index+1] == "Competitive" or choices[cursor.index+1] == "ECFA" or choices[cursor.index+1] == "StomperZ" then
-					self:stoptweening():linear(0.25):diffusealpha(1)
-				else
-					self:stoptweening():linear(0.25):diffusealpha(0)
-				end
-			end
-		end,
-		-- lifemeter white border
-		Def.Quad{
-			InitCommand=function(self) self:zoomto(60,16):xy(68,-64) end
-		},
-		-- lifemeter black bg
-		Def.Quad{
-			InitCommand=function(self) self:zoomto(58,14):xy(68,-64):diffuse(0,0,0,1) end
-		},
-		-- lifemeter colored quad
-		Def.Quad{
-			InitCommand=function(self) self:zoomto(40,14):xy(59,-64):diffuse( GetCurrentColor() ) end
-		},
-		-- life meter animated swoosh
-		LoadActor(THEME:GetPathB("ScreenGameplay", "underlay/PerPlayer/LifeMeter/swoosh.png"))..{
-			InitCommand=function(self) self:zoomto(40,14):diffusealpha(0.45):xy(59,-64) end,
-			OnCommand=function(self)
-				self:customtexturerect(0,0,1,1):texcoordvelocity(-2,0)
-			end,
-		},
+		InitCommand=cmd(x,SCREEN_CENTER_X+100;y,SCREEN_CENTER_Y+13;z,-100;zoom,1.3);
+		LoadActor("char")..{
+			InitCommand=cmd(zoom,0.55;glow,color("1,1,1,0");diffusealpha,0;linear,0.3;glow,color("1,1,1,1");sleep,0.001;diffusealpha,1;linear,0.3;glow,color("1,1,1,0"));
+			MadeChoiceP1MessageCommand=cmd(playcommand,"GoOff");
+			MadeChoiceP2MessageCommand=cmd(playcommand,"GoOff");
+			GoOffCommand=cmd(sleep,.2;linear,0.3;diffusealpha,0;);
+		};
 	},
 }
-
-t[#t+1] = LoadActor("./GameplayDemo.lua" )
 
 return t
--- BGAnimations/ScreenSelectStyle underlay/default.lua
+++ BGAnimations/ScreenSelectStyle underlay/default.lua
@@ -1,6 +1,6 @@
 
 return Def.ActorFrame{
-	InitCommand=cmd(x,SCREEN_CENTER_X+100;y,SCREEN_CENTER_Y+13;z,-100;zoom,1.6);
+	InitCommand=cmd(x,SCREEN_CENTER_X+100;y,SCREEN_CENTER_Y+13;z,-100;zoom,1.3);
 	LoadActor("char")..{
 		InitCommand=cmd(zoom,0.55;glow,color("1,1,1,0");diffusealpha,0;linear,0.3;glow,color("1,1,1,1");sleep,0.001;diffusealpha,1;linear,0.3;glow,color("1,1,1,0"));
 			MadeChoiceP1MessageCommand=cmd(playcommand,"GoOff");
