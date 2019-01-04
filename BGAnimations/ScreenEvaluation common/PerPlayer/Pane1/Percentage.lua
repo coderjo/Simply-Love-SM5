cc66ea7c110f049163c291502a329fcd3b02a3b8
StefanoFiumara <sdf10517@gmail.com>
2019-01-03 20:57:16 -0600

    Add ECFA 2019 scoring calculations to screen evaluation

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/Percentage.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/Percentage.lua
@@ -4,6 +4,16 @@ local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn)
 local actualDP = stats:GetActualDancePoints()
 local PercentDP = stats:GetPercentDancePoints()
 local percent = FormatPercentScore(PercentDP)
+
+local num_W1 = stats:GetTapNoteScores("TapNoteScore_W1")
+local bad_scores = stats:GetTapNoteScores("TapNoteScore_Miss") + 
+				   stats:GetTapNoteScores("TapNoteScore_HitMine") +
+				   stats:GetHoldNoteScores("HoldNoteScore_LetGo")
+
+local ecfa_points = num_W1 - (bad_scores * 0.5)
+
+local ecfa_plus_points = string.format("%.2f", stats:GetPercentageOfTaps("TapNoteScore_W1") * 100)
+
 -- Format the Percentage string, removing the % symbol
 percent = percent:gsub("%%", "")
 
@@ -28,7 +38,7 @@ return Def.ActorFrame{
 
 	LoadFont("_wendy white")..{
 		Text="EX: ",
-		Name="PointsLabel",
+		Name="TournamentPointsLabel",
 		InitCommand=cmd(valign, -3.4; horizalign, left; zoom,0.1 ),
 		OnCommand=function(self)
 			self:x(15)
@@ -49,5 +59,46 @@ return Def.ActorFrame{
 			self:targetnumber(actualDP)
 		end
 
-	}
+	},
+
+	LoadFont("_wendy white")..{
+		Text="ECFA: ",
+		Name="ECFAPointsLabel",
+		InitCommand=cmd(valign, -3.4; horizalign, left; zoom,0.1 ),
+		OnCommand=function(self)
+			self:x(7)
+			self:diffusealpha( SL.Global.GameMode == "Competitive" and 1 or 0 )
+		end
+	},
+	LoadFont("_wendy white") .. {
+		Text=ecfa_points,
+		Name="ECFAPointsValue",
+		InitCommand=function(self)
+			self:diffusealpha( SL.Global.GameMode == "Competitive" and 1 or 0 )
+			self:valign(-3.4)
+			self:horizalign(right)
+			self:zoom(0.1)
+			self:x(67)
+		end,
+	},
+	LoadFont("_wendy white")..{
+		Text="ECFA: ",
+		Name="ECFAPointsLabel",
+		InitCommand=cmd(valign, -3.4; horizalign, left; zoom,0.1 ),
+		OnCommand=function(self)
+			self:x(7)
+			self:diffusealpha( SL.Global.GameMode == "ECFA" and 1 or 0 )
+		end
+	},
+	LoadFont("_wendy white") .. {
+		Text=ecfa_plus_points,
+		Name="ECFAPointsValue",
+		InitCommand=function(self)
+			self:diffusealpha( SL.Global.GameMode == "ECFA" and 1 or 0 )
+			self:valign(-3.4)
+			self:horizalign(right)
+			self:zoom(0.1)
+			self:x(67)
+		end,
+	},
 }
